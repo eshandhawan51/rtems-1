02f9fca700dcae12397dc7f442c32ea12815b020
Joel Sherrill <joel@rtems.org>
2020-03-12 15:00:08 -0500

    pthreadgetcpuclockid.c: Implement pthread_getcpuclockid to return thread Id

--- cpukit/posix/src/pthreadgetcpuclockid.c
+++ cpukit/posix/src/pthreadgetcpuclockid.c
@@ -8,8 +8,10 @@
 
 /*
  *  20.1.6 Accessing a Thread CPU-time Clock, P1003.4b/Draft 8, p. 58
- *
- *  COPYRIGHT (c) 1989-2007.
+ */
+
+/*
+ *  COPYRIGHT (c) 1989-2007,2020.
  *  On-Line Applications Research Corporation (OAR).
  *
  *  The license and distribution terms for this file may be
@@ -24,12 +26,31 @@
 #include <pthread.h>
 #include <errno.h>
 
-#include <rtems/seterr.h>
+#include <rtems/score/threadimpl.h>
 
 int pthread_getcpuclockid(
-  pthread_t    pid,
+  pthread_t    thread,
   clockid_t   *clock_id
 )
 {
-  rtems_set_errno_and_return_minus_one( ENOSYS );
+  Thread_Control               *the_thread;
+  ISR_lock_Context              lock_context;
+
+  if ( clock_id == NULL ) {
+    return EINVAL;
+  }
+
+  the_thread = _Thread_Get( thread, &lock_context );
+
+  if ( the_thread == NULL ) {
+    return ESRCH;
+  }
+
+  _Thread_State_acquire_critical( the_thread, &lock_context );
+
+  *clock_id = the_thread->Object.id;
+
+  _Thread_State_release( the_thread, &lock_context );
+
+  return 0;
 }
